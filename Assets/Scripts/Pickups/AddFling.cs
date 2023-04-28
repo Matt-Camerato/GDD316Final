587f33b1b8e2c936399119a569605f5f03d6e461
Matt-Camerato <matt.camerato@gmail.com>
2023-04-28 10:44:31 -0400

    Tunnel Generation Started
    Added 3D asset pack and started creating tunnel pieces which will be used to procedurally generate an infinite tunnel.

--- Assets/Scripts/Pickups/AddFling.cs
+++ Assets/Scripts/Pickups/AddFling.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AddFling : Pickup
 {
     [SerializeField] private int amountOfFlingsToAdd;
+    [SerializeField] private TMP_Text flingNumText;
+
+    private void Start() => flingNumText.text = "+" + amountOfFlingsToAdd;
     
     protected override void OnPickUp(PickupManager playerController)
     {
