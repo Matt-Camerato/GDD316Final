b5c8517fe9f7cb0abda8d176e634c1caeb1e966c
Matt-Camerato <matt.camerato@gmail.com>
2023-05-02 17:52:52 -0400

    Main Menu Screen Created

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void BeginGame()
+    {
+
+    }
+
+    public void Settings()
+    {
+
+    }
+
+    public void Return()
+    {
+        
+    }
+}
